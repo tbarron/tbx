b4b386f2a23e6eb8dab1327c457d033904a9513c
Tom Barron <tom.barron@comcast.net>
2016-01-06 05:50:57 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+# with open('HISTORY.rst') as history_file:
+#     history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='tbx',
+    # version=version.__version__,
+    description="toolbox",
+    long_description=readme,
+    author="Tom Barron",
+    author_email='tusculum@gmail.com',
+    url='https://github.com/tbarron/tbx',
+    packages=[
+        'tbx',
+    ],
+    package_dir={'tbx':
+                 '.'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="ISCL",
+    zip_safe=False,
+    keywords='tbx',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='test_tbx',
+    tests_require=test_requirements,
+)
