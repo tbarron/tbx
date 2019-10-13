df949d0d3589b66de5680299661aca52798d4702
Tom Barron <tusculum@gmail.com>
2019-10-13 15:23:06 -0400

    Update get_this() to just hard-code the poem as a list of strings

--- test/test_tbx.py
+++ test/test_tbx.py
@@ -720,19 +720,39 @@ def ctest(tmpdir):
 # -----------------------------------------------------------------------------
 def get_this():
     """
-    return The Zen of Python as a string
+    return The Zen of Python as a list of strings
     """
     while True:
         try:
             rval = get_this.zen
             return rval
         except AttributeError:
-            buf = io.StringIO()
-            orig = sys.stdout
-            sys.stdout = buf
-            import this    # noqa
-            sys.stdout = orig
-            get_this.zen = buf.getvalue()
+            get_this.zen = [
+                "The Zen of Python, by Tim Peters",
+                "Beautiful is better than ugly.",
+                "Explicit is better than implicit.",
+                "Simple is better than complex.",
+                "Complex is better than complicated.",
+                "Flat is better than nested.",
+                "Sparse is better than dense.",
+                "Readability counts.",
+                "Special cases aren't special enough to break the rules.",
+                "Although practicality beats purity.",
+                "Errors should never pass silently.",
+                "Unless explicitly silenced.",
+                "In the face of ambiguity, refuse the temptation to guess.",
+                "There should be one-- and preferably only one --obvious"
+                " way to do it.",
+                "Although that way may not be obvious at first unless"
+                " you're Dutch.",
+                "Now is better than never.",
+                "Although never is often better than *right* now.",
+                "If the implementation is hard to explain, it's a bad idea.",
+                "If the implementation is easy to explain, it may be a"
+                " good idea.",
+                "Namespaces are one honking great idea -- let's do"
+                " more of those!",
+            ]
 
 
 # -----------------------------------------------------------------------------
