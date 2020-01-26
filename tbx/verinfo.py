8b83e6b7b8fd56d64224c61fc6c541403e16979f
Tom Barron <tusculum@gmail.com>
2020-01-26 08:31:40 -0500

    Update CHANGELOG.md, verinfo.py for release 1.1.7

--- tbx/verinfo.py
+++ tbx/verinfo.py
@@ -1,4 +1,3 @@
-_v = '1.1.6'
 """
 This file is named verinfo.py (rather than version.py) so that its name does
 not conflict at import with the tbx method version()
@@ -6,3 +5,4 @@ not conflict at import with the tbx method version()
 This is free and unencumbered software released into the public domain.
 For more information, please refer to <http://unlicense.org/>
 """
+_v = '1.1.7'
